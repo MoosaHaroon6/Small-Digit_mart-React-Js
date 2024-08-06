336d8579918156162b9c21b310ab0741c9bedb22
Moosa Haroon <moosaharoon6nov@gmail.com>
2024-08-06 07:22:01 +0500

    counter app

--- src/app/categories.tsx
+++ src/app/categories.tsx
@@ -6,21 +6,27 @@ type CategoriesType = {
 };
 
 export default function Categories({ categories, selectCategory }: CategoriesType) {
-
     return (
         <div
             style={{
-                display: 'flex', justifyContent: 'space-evenly', width: '400px',
-               margin:'auto', flexWrap: 'wrap', height: '150px',marginTop:'40px'  
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                width: '400px',
+                margin: 'auto',
+                flexWrap: 'wrap',
+                height: '150px',
+                marginTop: '40px'
             }}>
             {
                 categories.map((category, i) => (
                     <button
                         key={category + i}
-                        style={{
-                            padding: '10px 20px', width: '180px', fontSize: '1.1rem',
-                            borderRadius: '5px', border: '1px solid gray', height: '40px'
-                        }}
+                        style={
+                            {
+                                padding: '10px 20px', width: '180px', fontSize: '1.1rem',
+                                borderRadius: '5px', border: '1px solid gray', height: '40px'
+                            }
+                        }
                         onClick={() => { selectCategory(category) }}>
                         {category}
                     </button>
